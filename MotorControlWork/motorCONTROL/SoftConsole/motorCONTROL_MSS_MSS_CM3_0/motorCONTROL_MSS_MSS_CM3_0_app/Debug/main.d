00ef3b85040bbdb229f200b425a1692428b9907c
Melinda Kothbauer <mkothbau@umich.edu>
2017-04-14 12:09:24 -0400

    working motor control. pid needs tuning.

---
+++ MotorControlWork/motorCONTROL/SoftConsole/motorCONTROL_MSS_MSS_CM3_0/motorCONTROL_MSS_MSS_CM3_0_app/Debug/main.d
@@ -0,0 +1,19 @@
+main.d main.o: ../main.c \
+ N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/mss_uart.h \
+ N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/a2fxxxm3.h \
+ N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/core_cm3.h \
+ N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/system_a2fxxxm3.h \
+ N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_gpio/mss_gpio.h \
+ N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_gpio/../../CMSIS/a2fxxxm3.h
+
+N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/mss_uart.h:
+
+N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/a2fxxxm3.h:
+
+N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/core_cm3.h:
+
+N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_uart/../../CMSIS/system_a2fxxxm3.h:
+
+N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_gpio/mss_gpio.h:
+
+N:\eecs373\PolyBots\MotorControlWork\motorCONTROL\SoftConsole\motorCONTROL_MSS_MSS_CM3_0\motorCONTROL_MSS_MSS_CM3_0_hw_platform/drivers/mss_gpio/../../CMSIS/a2fxxxm3.h:
